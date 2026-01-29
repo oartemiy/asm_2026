71e99ab19b1e20e127a6e224f9aef6122e78298a
oartemiy <artemiy.obraztsov@yandex.ru>
2026-01-29 13:50:06 +0000

    01-5

--- main.asm
+++ main.asm
@@ -4,11 +4,12 @@ section .text
 global main
 main:
     GET_UDEC 4, eax
-    GET_UDEC 4, ebx
+    GET_UDEC 4, ecx
 
-    ror dword eax, ebx
+    ror eax, cl
 
     PRINT_UDEC 4, eax
     NEWLINE
+    
     xor eax , eax
     ret
